69bba3da7bb0bd18bec97a56cdae59e97d0ce8c2
Ishaq Ali <ishaqniloy1996@gmail.com>
2021-02-14 00:21:31 +0600

    Updated php file

--- php/contact.php
+++ php/contact.php
@@ -1,5 +1,11 @@
  <?php
  if(isset($_POST['name'], $_POST['email'], $_POST['subject'], $_POST['message'])){
- 	echo "Your name is ".$_POST[name];
+     $to = 'ishaqniloy1996@gmail.com';
+     $from = $_POST['email'];
+     $subject = $_POST['subject'];
+     $message = "From: ".$from."\n".$_POST['message'];
+     $headers = "From: ".$from;
+     
+ 	 mail($to, $subject, $message, $headers);
  }
  
