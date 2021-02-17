7779b3fdacf5b831244f0d854be4705e817121a0
Ishaq Ali <ishaqniloy1996@gmail.com>
2021-02-17 17:33:10 +0600

    Updated JavaScript file

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
+ var flag = 0;
+
 function closeModal() {
     document.getElementById("my-modal").style.display = "none";
 }
+
+function toggleNum() {
+    if(flag == 0){
+        document.getElementById("phone-number").style.display = "none";
+        flag = 1;
+    }
+    else{
+        document.getElementById("phone-number").style.display = "block";
+        flag = 0;
+    }
+}
+
