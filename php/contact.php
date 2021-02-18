dd6f38aaacc98cee68645150fb1aa91bf4f77ef0
Ishaq Ali <ishaqniloy1996@gmail.com>
2021-02-19 02:22:59 +0600

    Updated contact.php file

--- php/contact.php
+++ php/contact.php
@@ -1,11 +1,11 @@
- <?php
- if(isset($_POST['name'], $_POST['email'], $_POST['subject'], $_POST['message'])){
-     $to = 'ishaqniloy1996@gmail.com';
-     $from = $_POST['email'];
-     $subject = $_POST['subject'];
-     $message = "From: ".$from."\n".$_POST['message'];
-     $headers = "From: ".$from;
-     
- 	 mail($to, $subject, $message, $headers);
- }
- 
+<?php
+if(isset($_POST['name'], $_POST['email'], $_POST['subject'], $_POST['message'])){
+    $name = $_POST['name'];
+    $to = 'ishaqniloy1996@gmail.com';
+    $from = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = "From: ".$name."\n"."Email: ".$from."\n\n".$_POST['message'];
+    $headers = "From: ".$from;
+
+    mail($to, $subject, $message, $headers);
+}
